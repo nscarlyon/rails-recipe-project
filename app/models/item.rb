474089a940131d71aaf643f834eb697f4e063899
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-02 16:34:46 +0000

    add validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   has_many :recipes
+
+  validates :name, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :ingredients
   has_many :items, through: :ingredients
+
+  validates :name, presence: true
 end
