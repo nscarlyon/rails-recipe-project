9a2abfb25985fc383f481351f7928d2fe429bfef
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-02 16:13:57 +0000

    add ingredient model and migration

---
+++ db/migrate/20160802161311_create_ingredients.rb
@@ -0,0 +1,11 @@
+class CreateIngredients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ingredients do |t|
+      t.integer :quantity
+      t.belongs_to :user
+      t.belongs_to :item
+
+      t.timestamps
+    end
+  end
+end
