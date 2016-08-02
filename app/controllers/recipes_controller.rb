ec9dde76f1b036b538b5f074d765821aa2c0975e
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-02 18:31:13 +0000

    fixed user recipes link

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,16 @@
 class RecipesController < ApplicationController
 
   def index
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+        if @user.nil?
+          redirect_to home_path, alert: "Artist not found"
+        else
+          @recipes = @user.recipes
+        end
+    else
+        @recipes = Recipe.all
+      end
   end
 
   def new
