ff313ff44c8790cf1f6d00485e0bb3d1114a39de
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-04 00:05:52 +0000

    user.most_recipes method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,15 @@ class User < ApplicationRecord
 
   has_many :recipes
 
-  
-
      def self.from_omniauth(auth)
        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
          user.email = auth.info.email
          user.password = Devise.friendly_token[0,20]
        end
      end
+
+     def self.most_recipes
+       User.joins(:recipes).group(:user_id).order("count(user_id) DESC").first
+     end
+
 end
