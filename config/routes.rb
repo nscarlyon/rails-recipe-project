350d9a4274cd7568a4fcbffb50e5372b75f7f2f6
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-04 00:41:21 +0000

    add ingredients controller and ingredients new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'ingredients/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home', as: 'home'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
