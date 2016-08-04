d07a23813ea2e0ac955c5ff1b295846e6aa052ef
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-04 01:34:54 +0000

    new ingredient form for recipe

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,11 @@
 class IngredientsController < ApplicationController
   def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = Ingredient.new
+    Item.new 
   end
+
+  def create
+  end
+
 end
