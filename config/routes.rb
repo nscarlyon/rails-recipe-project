39c87a6a67f1790f3f540997ae0022d59c679192
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-04 00:45:35 +0000

    add nested recipes ingredients routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'ingredients/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home', as: 'home'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
@@ -8,7 +6,10 @@ Rails.application.routes.draw do
   resources :users do
     resources :recipes, only: [:index, :show]
   end
-  resources :recipes
+
+  resources :recipes do
+    resources :ingredients, only: [:new, :create]
+  end
 
   get 'users/most_recipes', to: 'users#show'
 end
