d5ed1a4cd1bf9bc22c38d2e837ebaaf7462c3413
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-18 05:06:13 +0000

    new comment form and comment_params method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,14 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
+
+
   def edit
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content, :user_id, :recipe_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-
-  get 'comments/edit'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home', as: 'home'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :users do
-    resources :recipes, only: [:index, :show]
+    resources :recipes, only: [:index]
   end
 
+
   resources :recipes do
-    resources :ingredients, only: [:new, :create]
+    resources :comments, only: [:new, :create]
   end
 
   post 'recipes/:id/ingredients/new', to: 'ingredients#create'
