4874156bda1442a3a5b9ded536acb3dab9f7a973
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-28 17:19:07 +0000

    create new comment object from request

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,7 +3,9 @@ var nextComments = function() {
   var nextId = parseInt($(".js-next").attr("data-id")) + 1;
   $.get("/recipes/" + nextId + "/comments", function(data) {
       data["comments"].forEach(function(comment) {
-        console.log(comment)
+        var comment = new Comment(comment)
+        var recipeComments = []
+        recipeComments.push(comment)
       })
   });
 }
