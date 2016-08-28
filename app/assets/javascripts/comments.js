415102e19b8d78921816ff8ada29cfddb30c3b4d
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-28 17:14:52 +0000

    add nextComments function; requests comments for next recipe

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,13 @@
+
+var nextComments = function() {
+  var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+  $.get("/recipes/" + nextId + "/comments", function(data) {
+      data["comments"].forEach(function(comment) {
+        console.log(comment)
+      })
+  });
+}
+
 $(function () {
-  $(".js-next").on("click", Recipe.nextRecipe)
+  $(".js-next").on("click", nextComments)
 })
