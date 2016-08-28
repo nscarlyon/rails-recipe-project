75f1d7e331f4e764f8ea10bf244211f00337665e
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-28 17:21:53 +0000

    create comment constructor

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,11 @@
+function Comment(attributes) {
+  this.id = attributes.id;
+  this.content = attributes.content;
+  this.recipe_id = attributes.recipe_id;
+  this.user_id = attributes.user_id;
+  this.user.email = attributes.user.email;
+}
+
 
 var nextComments = function() {
   var nextId = parseInt($(".js-next").attr("data-id")) + 1;
