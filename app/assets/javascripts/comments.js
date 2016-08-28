eb438113877558ade1b1969df1e4d3f4e021485a
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-28 20:35:15 +0000

    add comments renderDisplay function

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -7,13 +7,17 @@ Comments.setTemplate = function() {
   Comments.template = Handlebars.compile(Comments.templateSource);
 }
 
+Comments.prototype.renderDisplay = function() {
+  return Comments.template(this)
+}
+
 var nextComments = function() {
   var nextId = parseInt($(".js-next").attr("data-id")) + 1;
 
   $.get("/recipes/" + nextId + "/comments", function(data) {
     Comments.setTemplate();
     var comments = new Comments(data)
-
+    var renderDisplay = comments.renderDisplay()
   })
 }
 
