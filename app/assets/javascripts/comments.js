c22017e8cb6f5f724e0c7a5cf69b1d59cbf152a3
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-28 17:31:23 +0000

    fix this.user in comment constructor; move var recipeComments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,18 +3,19 @@ function Comment(attributes) {
   this.content = attributes.content;
   this.recipe_id = attributes.recipe_id;
   this.user_id = attributes.user_id;
-  this.user.email = attributes.user.email;
+  this.user = attributes.user;
 }
-
 
 var nextComments = function() {
   var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+  var recipeComments = []
+
   $.get("/recipes/" + nextId + "/comments", function(data) {
       data["comments"].forEach(function(comment) {
         var comment = new Comment(comment)
-        var recipeComments = []
         recipeComments.push(comment)
       })
+      console.log(recipeComments)
   });
 }
 
